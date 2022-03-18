d72a237a2fd843cbe5284cdd43d44c57d1fc61cd
psj <271533323@qq.com>
2022-03-18 20:44:40 +0800

    123

---
+++ index.js
@@ -0,0 +1,44 @@
+const obj = {
+    j_vas: {
+        s_v_g: [{ _s_g_s: [1, 2, 3, { s_s: 3 }] }],
+        _javasc: {
+            m1: () => { }
+        }
+    }
+}
+
+/**
+ * 将对象或者数组中带有下划线key的转化为驼峰类型
+ */
+
+function objectToUpperCase(obj) {
+    const isObject = isOriginObject(obj);
+    if (isObject) {
+        const isArray = Array.isArray(obj)
+        if (isArray) {
+            return obj.map(objectToUpperCase)
+        } else {
+            const newObject = {}
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    const value = obj[key]
+                    newObject[camelToUpperCase(key)] = objectToUpperCase(value)
+                }
+            }
+            return newObject
+        }
+    } else {
+        return obj
+    }
+}
+
+function isOriginObject(v) {
+    return typeof v === 'object' && v !== null
+}
+
+function camelToUpperCase(str) {
+    return str.replace(/_[a-z]/g, s => s[1].toUpperCase())
+}
+
+console.log(objectToUpperCase(obj));
+console.log(objectToUpperCase(obj.j_vas.s_v_g));
